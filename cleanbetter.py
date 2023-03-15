46b2f56f7f2fe3fcb90d87213f2ba48ac142f14f
cLEANTHUGGcODES <127801194+cLEANTHUGGcODES@users.noreply.github.com>
2023-03-14 20:11:14 -0500

    Update cleanbetter.py

--- cleanbetter.py
+++ cleanbetter.py
@@ -1,6 +1,17 @@
-import datetime
+import pandas as pd
 
-input_file = r'C:\Users\James\Desktop\input.xlsx'
-current_date = datetime.datetime.now().strftime('%m.%d.%Y')
-output_file = fr'C:\Users\James\Desktop\OptumRx Accumulations {current_date}.xlsx'
-image_file = fr'C:\Users\James\Desktop\Vendor_Breakdown_No_Optum_{current_date}.png'
+def read_input_file(file_path):
+    return pd.read_excel(file_path)
+
+def drop_columns(df, columns_to_remove):
+    ...
+    return df
+
+def sort_and_filter(df):
+    ...
+    return df
+
+def format_columns(df):
+    ...
+    return df
+
