5313208ded4dc1912a0bc4a1c175a4f6f2d4cd4a
cLEANTHUGGcODES <127801194+cLEANTHUGGcODES@users.noreply.github.com>
2023-03-14 20:11:39 -0500

    Update cleanbetter.py

--- cleanbetter.py
+++ cleanbetter.py
@@ -1,17 +1,6 @@
 import pandas as pd
 
-def read_input_file(file_path):
-    return pd.read_excel(file_path)
-
-def drop_columns(df, columns_to_remove):
+def save_dataframe_to_excel(df, output_file):
     ...
-    return df
-
-def sort_and_filter(df):
-    ...
-    return df
-
-def format_columns(df):
-    ...
-    return df
+    return writer
 
